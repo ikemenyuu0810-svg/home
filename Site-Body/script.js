c825b837dfd02ab6755a9078813a3ddc7ad3c769
fukufukuyuu <ikemenyuu0810@gmail.com>
2026-01-11 18:29:50 +0900

    a

--- Site-Body/script.js
+++ Site-Body/script.js
@@ -1321,7 +1321,7 @@ function initGlobe() {
   const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.3);
   globeScene.add(ambientLight);
 
-  loadGlobeSVGTexture('./earth.svg');
+  loadGlobeSVGTexture('./SVG-IMG/earth-SVG/earth.svg');
 }
 
 function loadGlobeSVGTexture(url) {
