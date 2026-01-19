9f577158bb757db14a9d3a6beb309afb1cd656c1
fukufukuyuu <ikemenyuu0810@gmail.com>
2026-01-20 01:02:23 +0900

    a

--- Site-Body/script.js
+++ Site-Body/script.js
@@ -1,6 +1,4 @@
-// ========================================
-// Supabase統合コード - script.jsの最初に追加
-
+// ====================supabase===================
 // 1. 設定（ここを変更してください）
 const SUPABASE_URL = 'https://lyupxfocvqqsmwagpicm.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_KXsg2JfUvG2YI5R5G7UjEg_FYJfFeoK';
